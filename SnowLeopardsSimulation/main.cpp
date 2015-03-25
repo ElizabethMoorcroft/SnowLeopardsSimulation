b18f22fd76bfcafe1cf13e7559be10d38a24120e
ElizabethMoorcroft <e.moorcroft@ucl.ac.uk>
2015-03-25 09:47:26 +0000

    Initial Commit

---
+++ SnowLeopardsSimulation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SnowLeopardsSimulation
+//
+//  Created by Elizabeth Moorcroft on 25/03/2015.
+//  Copyright (c) 2015 Elizabeth Moorcroft. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
